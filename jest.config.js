935b21e6b8c4cbff4d99e20f4ccdf54aa7552737
Mateus Siqueira Carneiro <mateussiqueiracarneiro@MacBook-Pro-de-Mateus.local>
2020-11-22 13:15:09 -0300

    chore: add jest to project and to eslint config

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    '<rootDir>/src'
+  ],
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: [
+    'lcov'
+  ],
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ]
+}
