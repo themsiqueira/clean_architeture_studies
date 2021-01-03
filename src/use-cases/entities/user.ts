65ceb0dbd205b432e3805096ed2bd4aac18c5cba
Mateus Siqueira Carneiro <mateussiqueiracarneiro@MacBook-Pro-de-Mateus.local>
2021-01-03 11:53:17 -0300

    feat: create user

--- src/use-cases/entities/user.ts
+++ src/use-cases/entities/user.ts
@@ -1,4 +1,4 @@
-import { Either, left } from '../../shared/either'
+import { Either, left, right } from '../../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { UserData } from './user-data'
 import { Email } from './email'
@@ -6,7 +6,15 @@ import { InvalidNameError } from './errors/invalid-name-error'
 import { Name } from './name'
 
 export class User {
-  static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.email = email
+    this.name = name
+  }
+
+  public static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
     if (nameOrError.isLeft()) {
       return left(new InvalidNameError())
@@ -17,5 +25,10 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
